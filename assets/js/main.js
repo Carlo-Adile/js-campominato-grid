436fcd0ab2447e81f5a244408708e62c44ded7a7
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-28 17:40:02 +0100

    add everything in a function campoMinato

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,60 +4,64 @@ const difficultyValue = document.getElementById("difficulty_value")
 
 const myGame = document.getElementById("my_game")
 
-document.getElementById("difficulty_select").addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const difficulty = difficultyValue.value;
-
-  switch (difficulty) {
-    case "1":
-      gridBox = 100;
-      break;
-    case "2":
-      gridBox = 81;
-      break;
-    case "3":
-      gridBox = 49;
-      break;
-    default:
-      gridBox = 100;
-      break;
-  }
-
-  /* esegui finchè myGame ha un figlio */
-  while (myGame.firstChild) {
-    myGame.removeChild(myGame.firstChild);
-  }
-
-  console.log(gridBox)
-
-  for (index = 1; index < gridBox + 1; index++) {
-    let box = document.createElement("div")
-
+function campoMinato(){
+  document.getElementById("generate_box").addEventListener('click', function (e) {
+    e.preventDefault();
+  
+    const difficulty = difficultyValue.value;
+  
     switch (difficulty) {
       case "1":
-        box.classList.add('box_easy')
+        gridBox = 100;
         break;
       case "2":
-        box.classList.add('box_medium')
+        gridBox = 81;
         break;
       case "3":
-        box.classList.add('box_hard')
+        gridBox = 49;
         break;
       default:
-        box.classList.add('box_easy')
+        gridBox = 100;
         break;
     }
-    box.innerHTML = (index)
+  
+    /* esegui finchè myGame ha un figlio */
+    while (myGame.firstChild) {
+      myGame.removeChild(myGame.firstChild);
+    }
+  
+    console.log(gridBox)
+  
+    for (index = 1; index < gridBox + 1; index++) {
+      let box = document.createElement("div")
+  
+      switch (difficulty) {
+        case "1":
+          box.classList.add('box_easy')
+          break;
+        case "2":
+          box.classList.add('box_medium')
+          break;
+        case "3":
+          box.classList.add('box_hard')
+          break;
+        default:
+          box.classList.add('box_easy')
+          break;
+      }
+      box.innerHTML = (index)
+  
+      myGame.appendChild(box);
+  
+      box.addEventListener('click', function () {
+        this.classList.toggle('azure');
+      });
+    }
+  
+  })
+}
 
-    myGame.appendChild(box);
-
-    box.addEventListener('click', function () {
-      this.classList.toggle('azure');
-    });
-  }
-
-})
+campoMinato();
 
 
 
