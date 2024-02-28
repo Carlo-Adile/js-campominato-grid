05d57873b063ff8f87054532d660891c50af93dd
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-28 16:18:57 +0100

    add number of box, toggle color function

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ document.getElementById("difficulty_select").addEventListener('submit', function
   
   console.log(gridBox)
 
-  for(index = 0; index < gridBox; index++){
+  for(index = 1; index < gridBox+1; index++){
     let box = document.createElement("div")
 
     switch(difficulty) {
@@ -43,12 +43,13 @@ document.getElementById("difficulty_select").addEventListener('submit', function
         box.classList.add('box_easy')
         break;
     }
+    box.innerHTML = (index)
 
     myGame.appendChild(box);
-    
-    /* box.classList.add('box')
-    box.innerHTML(index) */
-    /* box.addEventListener() */
+
+    box.addEventListener('click', function () {
+      this.classList.toggle('azure');
+    });
   }
 
 })
@@ -57,30 +58,3 @@ document.getElementById("difficulty_select").addEventListener('submit', function
 
 
 
-/* function createBox(){
-  for (let index = 0; index < gridRow; index++) {
-    for (let index2 = 0; index2 < gridCol; index2++) {
-      let box = document.createElement("div");
-      box.classList.add('box');
-      
-
-      box.addEventListener('click', function () {
-        this.classList.toggle('green');
-      });
-  
-      myContainer.appendChild(box);
-    }
-  
-  }
-} */
-
-/* const nameElement = document.getElementById("ticket_name");
-const kmElement = document.getElementById("ticket_km");
-const ageElement = document.getElementById("ticket-age"); */
-
-
-
-
-/* document.getElementById("generate_ticket").addEventListener('submit',
-  function(e){
-    e.preventDefault(); */
