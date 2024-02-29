e76563c53584881f0e21156ea1379c767ee8d968
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-29 12:21:00 +0100

    add system to detect click on bombs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,21 @@
+let bombList = []
 /* riferimento ad HTML */
 const difficultyValue = document.getElementById("difficulty_value")
 const myGame = document.getElementById("my_game")
-let bombList = []
+
 let gridBox = 0;
 
 /* click sul bottone per avviare */
 document.getElementById("generate_box").addEventListener('click', function (e) {
   e.preventDefault();
   pickDifficulty()
-  
+
 })
 
 /* determina la difficoltà */
 function pickDifficulty() {
   const difficulty = difficultyValue.value;
-  
+
   switch (difficulty) {
     case "1":
       gridBox = 100;
@@ -41,7 +42,8 @@ function pickDifficulty() {
 
 /* add 16 bombs */
 /* produci il numero casuale */
-function getRandomNumb(min, max){
+function getRandomNumb(min, max) {
+
   /* check */
   console.log("random numb is reading gridBox: " + gridBox)
   min = 1;
@@ -49,18 +51,18 @@ function getRandomNumb(min, max){
   let mushroomCount = 0;
   let avoidLoop = 0;
 
-  while(mushroomCount < 16){
+  while (mushroomCount < 16) {
     let numbToAdd = parseInt(Math.random() * (max - min + 1) + min);
     console.log("random numb is " + numbToAdd)
 
-    if(bombList.includes(numbToAdd) === false){
+    if (bombList.includes(numbToAdd) === false) {
       bombList.push(numbToAdd);
       mushroomCount++;
     }
-    
+
     console.log(bombList)
   }
-  
+  return bombList
 }
 
 /* crea le box */
@@ -69,6 +71,7 @@ function campoMinato(difficulty) {
   /* esegui finchè myGame ha un figlio */
   while (myGame.firstChild) {
     myGame.removeChild(myGame.firstChild);
+
   }
 
   for (index = 1; index < gridBox + 1; index++) {
@@ -90,30 +93,30 @@ function campoMinato(difficulty) {
     }
 
     /* gestisci il contenuto e il toggle */
-    if(bombList.includes(index)){
+    if (bombList.includes(index)) {
       box.innerHTML = ("bomb")
+      box.classList.add('bomb')
     }
-    else{
+    else {
       box.innerHTML = (index)
     }
-    
-    
-    
-   
+
     myGame.appendChild(box);
     box.addEventListener('click', function () {
       this.classList.toggle('azure');
-      
     });
-
+    
   }
-  
+  /* verifica quando l'utente clicka su una bomba */
+  const theBombs = document.querySelectorAll('.bomb');
+    theBombs.forEach(bomb => {
+      bomb.addEventListener('click', event => {
+        console.log('Element clicked:', bomb);
+      });
+
+    });
 }
 
- /* generate bomb list */
- /* for(index = 0; index < bombList.length; index++){
-  let placeHere = bombList[index]
-  box.innerHTML
-  console.log("place a bomb at box " + placeHere)
- } */
+
+
 
